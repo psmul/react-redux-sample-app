f5b9c0920b6049d9114bf8fdbaaf15dab8318749
Patryk Smul <patryksmul@outlook.com>
2019-06-05 17:47:07 +0200

    changes for receiving and saving user objects from external API

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,6 +2,7 @@ import BaseService from './BaseService';
 import config from '../config/config.dev';
 import { plainToClass } from 'class-transformer';
 import UserModel from '../models/UserModel';
+import axios from 'axios';
 
 export class UserService {
 
@@ -14,16 +15,7 @@ export class UserService {
   fetchUsersFromExternalApi(resultsCount) {
     const resultsQuery = resultsCount || 10;
     return BaseService.get(config.API.EXTERNAL + `?results=${resultsQuery}&inc=name,email,login,picture`).then((data) => {
-      return data.data.results.map((entity, idx) => {
-        return plainToClass(UserModel, {
-          id: entity.id,
-          firstName: entity.name.first,
-          lastName: entity.name.last,
-          email: entity.email,
-          uuid: entity.login.uuid,
-          pictureUrl: entity.picture.thumbnail
-        });
-      })
+      return this.mapAndSaveUsersToLocalDB(data.data.results)
     });
   }
 
@@ -39,6 +31,29 @@ export class UserService {
     return BaseService.delete(config.API.LOCAL + `${this.resource}/${user.id}`);
   }
 
+  mapAndSaveUsersToLocalDB(data) {
+    const result = data.map((entity) => {
+      return plainToClass(UserModel, {
+        id: entity.id,
+        firstName: entity.name.first,
+        lastName: entity.name.last,
+        email: entity.email,
+        uuid: entity.login.uuid,
+        pictureUrl: entity.picture.thumbnail
+      });
+    });
+
+    return Promise.all(
+      result.map((user) => {
+        return axios.post(config.API.LOCAL + this.resource, user);
+      })
+    )
+    .then((data) => {
+      return data.map((resData) => {
+        return plainToClass(UserModel, resData.data);
+      })
+    })
+  }
 
 
 }
